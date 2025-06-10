39f929c74f069fae00172dc75f6b4367dc734ead
WilliamFloydJ <floydfamily71@gmail.com>
2025-06-10 01:04:28 -0600

    main

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import "./CSS/App.css";
 import "./CSS/Header.css";
 import "./CSS/Lure.css";
 import "./CSS/White.css";
+import "./CSS/Feeling.css";
+import "./CSS/Footer.css";
 import Header from "./components/Header.jsx";
 import AppMainBody from "./components/AppMainBody.jsx";
 import Lure from "./components/Lure.jsx";
 import Feeling from "./components/Feeling.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
@@ -17,6 +20,7 @@ function App() {
       </div>
       <Lure />
       <Feeling />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="body-footer">
+      <div className="body-footer-main">
+        <h1>LETS TALK</h1>
+        <p>
+          Sed varius, tellus nec pharetra lobortis, metus ante placerat nunc,
+          fermentum lobortis tortor nisl vitae dolor. Sed efficitur diam a ipsum
+          tempus vulputate.
+        </p>
+        <div>
+          <div className="footer-bar">
+            <div className="footer-links"></div>
+            <div className="contact-links"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
