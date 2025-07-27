857669d3ae3331772198195bd431664a38443f33
WilliamFloydJ <floydfamily71@gmail.com>
2025-07-27 15:05:58 -0600

    working

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,5 @@
 import "../CSS/Services.css";
+import ServicesContainer from "./ServicesContainer";
 
 const Services = () => {
   return (
@@ -6,91 +7,7 @@ const Services = () => {
       <div className="services-back"></div>
       <h1>What’s included when we work together?</h1>
       <p>Click each service to learn more</p>
-      <div className="services-container">
-        <div className="services-grid">
-          <div className="services-li">
-            <a href="/Services/0">
-              <div className="services-li-img">
-                <img src="/Services/Retirement.svg" />
-              </div>
-              <h3>Retirement Planning</h3>
-            </a>
-          </div>
-          <div className="services-li">
-            <a href="/Services/1">
-              <div className="services-li-img">
-                <img src="/Services/Tax.svg" />
-              </div>
-              <h3>Tax Planning</h3>
-            </a>
-          </div>
-          <div className="services-li">
-            <a href="/Services/2">
-              <div className="services-li-img">
-                <img src="/Services/Investment.svg" />
-              </div>
-              <h3>Investment Planning</h3>
-            </a>
-          </div>
-          <div className="services-li">
-            <a href="/Services/3">
-              <div className="services-li-img">
-                <img src="/Services/Insurance.svg" />
-              </div>
-              <h3>Insurance Planning</h3>
-            </a>
-          </div>
-          <div className="services-li">
-            <a href="/Services/4">
-              <div className="services-li-img">
-                <img src="/Services/Estate.svg" />
-              </div>
-              <h3>Legacy Planning</h3>
-            </a>
-          </div>
-          <div className="services-li">
-            <a href="/Services/5">
-              <div className="services-li-img">
-                <img src="/Services/Employee-Benefits.svg" />
-              </div>
-              <h3>Employee Benefits</h3>
-            </a>
-          </div>
-          <div className="services-li">
-            <a href="/Services/6">
-              <div className="services-li-img">
-                <img src="/Services/Financial-Therapy.svg" />
-              </div>
-              <h3>Financial Therapy</h3>
-            </a>
-          </div>
-          <div className="services-li">
-            <a href="/Services/7">
-              <div className="services-li-img">
-                <img src="/Services/Life-Transition.svg" />
-              </div>
-              <h3>Life Transition Planning</h3>
-            </a>
-          </div>
-          <div className="services-li">
-            <a href="/Services/8">
-              <div className="services-li-img">
-                <img src="/Services/Business.svg" />
-              </div>
-              <h3>Business Planning</h3>
-            </a>
-          </div>
-          <div></div>
-          <div className="services-li">
-            <a href="/Services/9">
-              <div className="services-li-img">
-                <img src="Services/Other-Topics.svg" />
-              </div>
-              <h3>Other Topics</h3>
-            </a>
-          </div>
-        </div>
-      </div>
+      <ServicesContainer />
     </div>
   );
 };
--- src/components/WorkingMainBody.jsx
+++ src/components/WorkingMainBody.jsx
@@ -1,19 +1,7 @@
 import { useEffect, useState } from "react";
+import ServicesContainer from "./ServicesContainer";
 
 const WorkingMainBody = () => {
-  const [colorHeight, setColorHeight] = useState(0);
-  const [scrollY, setScrollY] = useState(0);
-  const ballHeight2 = 254;
-  const ballHeight3 = 597;
-
-  useEffect(() => {
-    window.addEventListener("scroll", setScrollFunc);
-  }, []);
-
-  const setScrollFunc = () => {
-    setScrollY(window.scrollY);
-  };
-
   return (
     <div className="working-body">
       <div className="working-back"></div>
@@ -34,28 +22,9 @@ const WorkingMainBody = () => {
             for free before making a decision:
           </p>
           <div className="working-list-items">
-            <div className="working-list-bar">
-              <div
-                className="working-list-barColor"
-                style={{ height: `${scrollY}px` }}
-              ></div>
-              <div className="working-list-ball working-list-ballColor"></div>
-              <div
-                className={
-                  `working-list-ball ` +
-                  (scrollY >= ballHeight2 ? "working-list-ballColor" : "")
-                }
-                style={{ top: `${ballHeight2}px` }}
-              ></div>
-              <div
-                className={
-                  "working-list-ball " +
-                  (scrollY >= ballHeight3 ? "working-list-ballColor" : "")
-                }
-                style={{ top: `${ballHeight3}px` }}
-              ></div>
-            </div>
+            <div className="working-list-bar"></div>
             <div className="working-list-row">
+              <h3>Step 1</h3>
               <div className="working-list-box">
                 <h2>Discovery Call</h2>
                 <p>
@@ -64,10 +33,9 @@ const WorkingMainBody = () => {
                   you in your journey.
                 </p>
               </div>
-              <h3>Step 1</h3>
             </div>
             <div className="working-list-row">
-              <h3 className="working-list-right">Step 2</h3>
+              <h3>Step 2</h3>
               <div className="working-list-box">
                 <h2>Follow-Up Email</h2>
                 <p>
@@ -81,6 +49,7 @@ const WorkingMainBody = () => {
               </div>
             </div>
             <div className="working-list-row">
+              <h3>Step 3</h3>
               <div className="working-list-box">
                 <h2>We Agree to Work Together</h2>
                 <p>
@@ -89,9 +58,122 @@ const WorkingMainBody = () => {
                   I would be honored to do so.
                 </p>
               </div>
-              <h3>Step 3</h3>
             </div>
           </div>
+        </div>
+        <div className="working-list working-services">
+          <div className="working-hr-div">
+            <hr />
+            <h3>What’s Included in the Process?</h3>
+            <hr />
+          </div>
+          <p>
+            New relationships begin with a three-month process where we take a
+            comprehensive look at each area of your financial life at a flat fee
+            of $5,100 per household. The reason I insist on doing a
+            comprehensive financial plan for each of my clients is because I
+            know about so many ways to optimize your finances which would
+            otherwise be ignored if we worked on a more limited engagement.
+          </p>
+          <h4>Click each service to learn more</h4>
+          <ServicesContainer />
+        </div>
+        <div className="working-list">
+          <div className="working-hr-div">
+            <hr />
+            <h3>Data Gathering</h3>
+            <hr />
+          </div>
+          <p>
+            The first step is gathering and organizing every piece of your
+            financial life. I will send you a questionnaire to help me get a
+            sense of who you are and where you’re at financially. During this
+            time, we also outline each of your financial pain points that you
+            would like to address.
+          </p>
+          <div className="working-list-items">
+            <div className="working-list-bar" style={{ left: "26%" }}></div>
+            <div className="working-list-row">
+              <h3>Meeting #1:</h3>
+              <div className="working-list-box">
+                <h2>Discovery Call</h2>
+                <p>
+                  Addressing Your Concerns Everyone has a reason for seeking out
+                  financial coaching, and we will make sure these issues get
+                  solved before moving on to anything else. We will discuss the
+                  recommendations to make sure they are a good fit and follow up
+                  on the progress in future meetings.
+                </p>
+              </div>
+            </div>
+            <div className="working-list-row">
+              <h3>Meeting #2:</h3>
+              <div className="working-list-box">
+                <h2>Describe Your Ideal Life</h2>
+                <p>
+                  As a financial coach, it is ultimately my job to make sure you
+                  are living your best life. Now that we have addressed your
+                  immediate concerns, what else would you change about your
+                  situation? Would you work less hours? Retire sooner? Spend
+                  more time traveling? Start a business? Own a giraffe? Each
+                  story is different and we will make a plan that you will be
+                  excited to pursue.
+                </p>
+              </div>
+            </div>
+            <div className="working-list-row">
+              <h3>Meeting #3:</h3>
+              <div className="working-list-box">
+                <h2>Presentation of the Plan</h2>
+                <p>
+                  Here’s where the magic happens. I will compile a comprehensive
+                  guide to each area of your financial life with my top
+                  recommendations for each area. During this meeting, I will
+                  teach you why each of these areas are important and ensure
+                  that you have a comfortable level of understanding as to why
+                  you should implement these strategies. We will also work
+                  together both during and after the meeting to customize the
+                  plan even further to your needs. I may be a financial expert,
+                  but you are the expert in your own life, and together we will
+                  make the best plan possible for your needs.
+                </p>
+              </div>
+            </div>
+            <div className="working-list-row">
+              <h3>Follow-Up : </h3>
+              <div className="working-list-box">
+                <h2>Follow-Up As Needed</h2>
+                <p>
+                  After the initial 3-month period, we will continue to follow
+                  up over the course of the next 2 months to ensure that your
+                  plan is off to a good start. During this time, we will
+                  communicate over email and we can schedule as many 30-minute
+                  calls as you would like (within reason) to address your
+                  questions. I will also send you nudges to ensure that the
+                  action items are either being accomplished or modified to make
+                  them more beneficial.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="working-list">
+          <div className="working-hr-div">
+            <hr />
+            <h3>Ongoing Engagements</h3>
+            <hr />
+          </div>
+          <p>
+            Clients who complete a comprehensive planning engagement may choose
+            to continue to enlist me as their guide. Pricing is calculated based
+            on the complexity of needs, with a minimum fee of $7,200 per year,
+            billed quarterly. Each year, there are 3 meetings offered in the
+            Spring, Summer, and Fall where we revisit the various areas of your
+            financial plan and make updates as needed. Additional meetings can
+            be added to address areas of financial therapy, transition planning,
+            or business planning, as needed.Unlimited communication by email or
+            scheduled calls is also included.
+          </p>
         </div>
       </div>
     </div>
